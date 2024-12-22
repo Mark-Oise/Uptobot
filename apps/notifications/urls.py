833001cf8f1ad9791d15fc0a66fd5c4ccffbb2df
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-12-22 22:11:03 +0100

    Update notifications urls

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -5,8 +5,6 @@ app_name = 'notifications'
 
 urlpatterns = [
     path('', views.notification_list, name='list'),
-    path('stream/', views.notification_stream, name='stream'),
     path('count/', views.notification_count, name='count'),
-    path('<int:pk>/mark-read/', views.mark_as_read, name='mark_as_read'),
-    path('mark-all-read/', views.mark_all_as_read, name='mark_all_as_read'),
+    path('mark-as-read/', views.mark_as_read, name='mark_as_read'),
 ]
