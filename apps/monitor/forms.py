989cd9018b1eb516c6a035b0eb9cfea047eaae40
Mark Oise <120611653+Mark-Oise@users.noreply.github.com>
2024-12-29 21:32:22 +0100

    Remove alert threshold to simplify logic for now

--- apps/monitor/forms.py
+++ apps/monitor/forms.py
@@ -7,13 +7,6 @@ class AddMonitorForm(forms.ModelForm):
     """
     Form for creating a new Monitor instance.
     """
-    alert_threshold = forms.IntegerField(
-        min_value=5,
-        max_value=60,
-        initial=60,
-        required=True,
-        widget=forms.NumberInput(attrs={'type': 'hidden'})  # We'll use the range input in the template
-    )
 
     class Meta:
         model = Monitor
@@ -21,24 +14,19 @@ class AddMonitorForm(forms.ModelForm):
             'name',
             'interval',
             'url',
-            'alert_threshold',
             'description',
         ]
 
     def clean(self):
         cleaned_data = super().clean()
         url = cleaned_data.get('url')
-        alert_threshold = cleaned_data.get('alert_threshold')
         interval = cleaned_data.get('interval')
 
         if not url:
             self.add_error('url', 'URL is required.')
         if not interval or interval < 5 or interval > 60:
             self.add_error('interval', 'Interval must be between 5 and 60 minutes.')
-        if alert_threshold is not None:
-            if alert_threshold < 5 or alert_threshold > 60:
-                self.add_error('alert_threshold', 'Alert threshold must be between 5 and 60 seconds.')
-
+        
         return cleaned_data
 
     def save(self, commit=True):
