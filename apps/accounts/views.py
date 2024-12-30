43ae49ca65511daa83072905247687432d13d3e2
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-12-30 15:57:30 +0100

    Update views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,36 +1,36 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from .forms import AccountUpdateForm, AlertSettingsForm
+from .forms import UserAccountUpdateForm, UserAlertSettingsForm
 from .models import UserAlertSettings
 
 
 # Create your views here.
 
 def settings_view(request):
-    # Get or create alert settings for the user
-    alert_settings, created = UserAlertSettings.objects.get_or_create(user=request.user)
+     # Get the alert settings for the current user
+    alert_settings = UserAlertSettings.objects.get(user=request.user)
     
     if request.method == 'POST':
         if 'alert_settings_form' in request.POST:
-            alert_form = AlertSettingsForm(request.POST, instance=alert_settings)
-            account_form = AccountUpdateForm(instance=request.user)
+            alert_form = UserAlertSettingsForm(request.POST, instance=alert_settings)
+            account_form = UserAccountUpdateForm(instance=request.user)
             if alert_form.is_valid():
                 alert_form.save()
-                messages.success(request, 'Account settings updated successfully.')
-                return redirect('settings')
+                messages.success(request, 'Alert settings updated successfully.')
+                return redirect('accounts:settings')
         else:
-            alert_form = AlertSettingsForm(instance=alert_settings)
-            account_form = AccountUpdateForm(request.POST, instance=request.user)
+            account_form = UserAccountUpdateForm(request.POST, instance=request.user)
+            alert_form = UserAlertSettingsForm(instance=alert_settings)
             if account_form.is_valid():
                 account_form.save()
-                messages.success(request, 'Alert settings updated successfully.')
-                return redirect('settings')
+                messages.success(request, 'Account settings updated successfully.')
+                return redirect('accounts:settings')
     else:
-        alert_form = AlertSettingsForm(instance=alert_settings)
-        account_form = AccountUpdateForm(instance=request.user)
+        alert_form = UserAlertSettingsForm(instance=alert_settings)
+        account_form = UserAccountUpdateForm(instance=request.user)
 
     context = {
-        'form': account_form,
+        'account_form': account_form,  # Renamed to be more explicit
         'alert_form': alert_form,
     }
-    return render(request, 'dashboard/monitor/settings.html', context)
+    return render(request, 'dashboard/settings.html', context)
