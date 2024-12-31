03ee8fcb335e4241d9cc79f560c449785e001c77
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-01-01 00:50:19 +0100

    New migrations: Refresh DB

--- apps/monitor/models.py
+++ apps/monitor/models.py
@@ -82,7 +82,7 @@ class Monitor(models.Model):
         verbose_name = 'Monitor'
         verbose_name_plural = 'Monitors'
         constraints = [
-            models.UniqueConstraint(fields=['user', 'url'], name='unique_user_url')
+            models.UniqueConstraint(fields=['user', 'url'], name='unique_user_url_monitor')
         ]
         indexes = [
             models.Index(fields=['slug']),
