1e9ad734a097a53523395021a345f61d39366b17
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-03-02 14:52:22 +0100

    Batched alert templates

--- apps/alerts/views.py
+++ apps/alerts/views.py
@@ -3,4 +3,4 @@ from django.conf import settings
 from django.shortcuts import render
 
 def email_incident(request):
-    return render(request, 'alerts/incident_email.html')
+    return render(request, 'alerts/batched_alerts.html')
