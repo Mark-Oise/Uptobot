04c6c87b83bc10d46eea8e257c1c8e0614077793
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2025-04-22 18:33:30 +0100

    Contact views

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
 def contact(request):
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            if request.user.is_authenticated:
+                contact.user = request.user
+            contact.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        initial_data = {}
+        if request.user.is_authenticated:
+            initial_data['email'] = request.user.email
+        form = ContactForm(initial=initial_data)
+    
+    return render(request, 'contact/contact.html', {'form': form})
